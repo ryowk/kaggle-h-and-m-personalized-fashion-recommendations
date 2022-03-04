47c818ef65ce4931f4fdc3aa171469cff97c98e1
ryowk <r.wakatsuki1207@gmail.com>
2022-03-04 10:36:29 +0900

    replace metric

--- metric.py
+++ metric.py
@@ -1,32 +1,68 @@
-# https://www.kaggle.com/george86/calculate-map-12-fast-faster-fastest#V3-Implementation!
+# https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/average_precision.py
 import numpy as np
 
 
-def map_at_k(y_true, y_pred, k=12):
+def apk(actual, predicted, k=12):
+    """
+    Computes the average precision at k.
 
-    # compute the Rel@K for all items
-    rel_at_k = np.zeros((len(y_true), k), dtype=int)
+    This function computes the average prescision at k between two lists of
+    items.
 
-    # collect the intersection indexes (for the ranking vector) for all pairs
-    for idx, (truth, pred) in enumerate(zip(y_true, y_pred)):
-        _, _, inter_idxs = np.intersect1d(truth, pred[:k], assume_unique=True, return_indices=True)
-        rel_at_k[idx, inter_idxs] = 1
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
 
-    # Calculate the intersection counts for all pairs
-    intersection_count_at_k = rel_at_k.cumsum(axis=1)
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
 
-    # we have the same denominator for all ranking vectors
-    ranks = np.arange(1, k + 1, 1)
+    """
+    if len(predicted) > k:
+        predicted = predicted[:k]
 
-    # Calculating the Precision@K for all Ks for all pairs
-    precisions_at_k = intersection_count_at_k / ranks
-    # Multiply with the Rel@K for all pairs
-    precisions_at_k = precisions_at_k * rel_at_k
+    score = 0.0
+    num_hits = 0.0
 
-    # Calculate the average precisions @ K for all pairs
-    average_precisions_at_k = precisions_at_k.mean(axis=1)
+    for i, p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i + 1.0)
 
-    # calculate the final MAP@K
-    map_at_k = average_precisions_at_k.mean()
+    if not actual:
+        return 0.0
 
-    return map_at_k
+    return score / min(len(actual), k)
+
+
+def mapk(actual, predicted, k=12):
+    """
+    Computes the mean average precision at k.
+
+    This function computes the mean average prescision at k between two lists
+    of lists of items.
+
+    Parameters
+    ----------
+    actual : list
+             A list of lists of elements that are to be predicted
+             (order doesn't matter in the lists)
+    predicted : list
+                A list of lists of predicted elements
+                (order matters in the lists)
+    k : int, optional
+        The maximum number of predicted elements
+
+    Returns
+    -------
+    score : double
+            The mean average precision at k over the input lists
+
+    """
+    return np.mean([apk(a, p, k) for a, p in zip(actual, predicted)])
