4b43ae16f4d65979d6e422c8277d63da8a3a5088
ryowk <r.wakatsuki1207@gmail.com>
2022-04-24 12:43:22 +0900

    wana

--- train_lfm.sh
+++ train_lfm.sh
@@ -7,12 +7,12 @@ for dataset in 1 10 100; do
     for week in 1 2 3 4 5; do
         for dim in 16; do
             for model_type in i_i; do
-                path=artifacts/lfm/lfm_${model_type}_dataset${dataset}_week${week}_dim${dim}_model.pkl
-                if [ -e $path ]; then
-                    echo skip $path
-                else
-                    python train_lfm.py $model_type $dataset $week $dim
-                fi
+                # path=artifacts/lfm/lfm_${model_type}_dataset${dataset}_week${week}_dim${dim}_model.pkl
+                # if [ -e $path ]; then
+                #     echo skip $path
+                #     continue
+                # fi
+                python train_lfm.py $model_type $dataset $week $dim
             done
         done
     done
