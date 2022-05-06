9d1154facf24d03c7ae06c67538c11799801076f
ryowk <r.wakatsuki1207@gmail.com>
2022-05-06 12:04:00 +0900

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-pip install lightfm lightgbm catboost faiss-cpu fire vaex kaggle logzero papermill
+pip install numpy pandas matplotlib lightfm lightgbm catboost faiss-cpu fire vaex kaggle logzero tqdm papermill
 
 mkdir -p $HOME/.kaggle
 cp kaggle.json $HOME/.kaggle/
