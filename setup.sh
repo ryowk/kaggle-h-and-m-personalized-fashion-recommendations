f5d95206d1371380e386fc61fc5362efe8189bab
ryowk <r.wakatsuki1207@gmail.com>
2022-05-06 11:57:10 +0900

    lightgbm catboost

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-pip install lightfm faiss-cpu fire vaex kaggle logzero papermill
+pip install lightfm lightgbm catboost faiss-cpu fire vaex kaggle logzero papermill
 
 mkdir -p $HOME/.kaggle
 cp kaggle.json $HOME/.kaggle/
